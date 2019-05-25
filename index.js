868ce041c5ceb042f3f413a4b711a4c16f3d85e7
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-05-25 18:13:51 +0200

    Main menu implementation and styling

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-360";
+import styles from "./styles";
+import destinations from "./data/destinations";
 
 export default class VrAdventureTraveller extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentDestinationId: 0,
+      destinations: destinations
+    };
+  }
+
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
+      <View style={stylesDefault.panel}>
+        <Text style={styles.welcomePanel}>React VR adventure traveller</Text>
+        <View style={styles.menuWrapper}>
+          {this.state.destinations.map(destination => {
+            return (
+              <View style={styles.menuItem} key={destination.id}>
+                <Text style={styles.menuItemText}>
+                  {destination.description}
+                </Text>
+              </View>
+            );
+          })}
         </View>
       </View>
     );
   }
-};
+}
 
-const styles = StyleSheet.create({
+const stylesDefault = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    justifyContent: "center",
+    alignItems: "center"
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
+    borderWidth: 2
   },
   greeting: {
-    fontSize: 30,
-  },
+    fontSize: 30
+  }
 });
 
-AppRegistry.registerComponent('VrAdventureTraveller', () => VrAdventureTraveller);
+AppRegistry.registerComponent(
+  "VrAdventureTraveller",
+  () => VrAdventureTraveller
+);
---
+++ styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-360";
+
+const styles = StyleSheet.create({
+  welcomePanel: {
+    backgroundColor: "#ef5350",
+    fontSize: 80,
+    fontWeight: "400",
+    layoutOrigin: [0.5, 0.5],
+    paddingLeft: 20,
+    paddingRight: 20,
+    textAlign: "center",
+    textAlignVertical: "center",
+    transform: [{ translate: [485, 120, -5] }]
+  },
+  menuWrapper: {
+    width: 800,
+    height: 150,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    transform: [{ translate: [-5, 120, -7.5] }]
+  },
+  menuItem: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 150,
+    height: 150,
+    borderRadius: 70,
+    borderWidth: 3,
+    borderColor: "#fff",
+    backgroundColor: "#ef5350"
+  },
+  menuItemText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff"
+  }
+});
+
+export default styles;
